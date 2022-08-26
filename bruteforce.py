a9dc18cb9fbc3517f0ee723bf524434532328a16
sabah <sabah.elaouni@gmail.com>
2022-08-26 12:10:12 +0200

    Final code adjustment and adding comments

--- bruteforce.py
+++ bruteforce.py
@@ -1,9 +1,15 @@
 import csv
 from itertools import combinations
 from time import time
+from tqdm import tqdm
 
 def read_csv_file():
-    with open("data_part1.csv") as csvfile:
+    """
+    :return : liste de données à analyser
+    Cette fonction permet de convertir le fichier csv en une liste qui contient
+    les même informations : nom de l'action, son cout et son bénefice
+    """
+    with open("dataset3.csv") as csvfile:
         data = csv.reader(csvfile, delimiter=',')
         data_list = []
         for row in data:
@@ -16,7 +22,7 @@ def calculate_profit(combination):
     profits = []
     for share in combination:
         profits.append(share[1] * share[2] / 100)
-
+    # retourner la somme des profits de la combinaison passée en paramètre
     return sum(profits)
 
 
@@ -24,18 +30,23 @@ def calculate_cost(combination):
     cost = []
     for share in combination:
         cost.append(share[1])
-
-
+    # retourner la somme des cout de la combinaison passée en paramètre
     return sum(cost)
-
 
 Max_invest = 500
 def best_combination(data_list):
+    """
+    :param data_list: la liste des données à anayser
+    :return: le meilleur profit, le Total investissement, la liste des actions
+    Cette fonction donne comme résultat le meilleur profit et la liste des actions les plus rentables
+    tout en respectant le montant total d'investissment de 500 euro, ce résultat est basé sur l'analyse de toutes
+    les combinaisons possibles d'actions.
+    """
     start = time()
     profit = 0
     best_comb = []
 
-    for i in range(len(data_list)):
+    for i in tqdm(range(len(data_list))):
         combs = combinations(data_list, i+1)
         # print(i+1)
         # print("test de combinaison",list(combs))
@@ -47,12 +58,16 @@ def best_combination(data_list):
                 total_profit = calculate_profit(comb)
 
                 if total_profit > profit:
+                    cost = total_cost
                     profit = total_profit
                     best_comb = comb
+
     print("Liste des actions les plus rentables : ")
+    print(len(best_comb),"actions")
     for i in best_comb:
         print(i)
-    print("le profil est de : ", profit)
+    print("-- le profil est de : ", profit)
+    print("-- Total investissement : ", cost)
     print(f'Temps d execution: {time() - start} seconds')
 
 
